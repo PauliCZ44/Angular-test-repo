dea0387c088eebf5001b1b07889623145c8fd97f
PauliCZ44 <stastnyp@seznam.cz>
2021-05-26 21:33:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-hgsxnp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-hgsxnp)
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { CourseService } from './course-service.service';
+import { CoursesComponent } from './courses/courses.component';
+import { StarComponent } from './star/star.component';
+import { TitleCaseInputComponent } from './title-case-input/title-case-input.component';
+import { TitleCasePipePipe } from './title-case-pipe.pipe';
+import { LikeComponent } from './like/like.component';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, CoursesComponent, StarComponent, TitleCaseInputComponent, TitleCasePipePipe, LikeComponent],
+  bootstrap: [AppComponent],
+  providers: [CourseService]
+})
+export class AppModule {}
